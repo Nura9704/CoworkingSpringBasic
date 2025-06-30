cd44f7cce18a2b97b8b887e36ea3e08a8177dd9f
Nura9704 <nurasil9704@gmail.com>
2025-06-30 22:06:31 +0500

    add new dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework:spring-context:6.2.7")
 }
 
 tasks.test {
